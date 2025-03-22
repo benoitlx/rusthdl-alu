3dae3ab3642eba28e615cadabc18df8c23a54fb6
benoitlx <benoitleroux41@gmail.com>
2025-03-22 13:59:04 +0100

    feat: add enum for Operation type

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,34 @@ use rust_hdl::prelude::*;
 
 const SIZE: usize = 16;
 
+#[derive(Copy, Clone, PartialEq, Debug, LogicState)]
+enum Operation {
+    Addition,
+    Substraction,
+    And,
+    Or,
+}
+
 #[derive(LogicBlock, Default)]
 struct ALU {
     pub a: Signal<In, Bits<SIZE>>,
     pub b: Signal<In, Bits<SIZE>>,
+    
+    // RustHDL will automatically adapt the number of wire needed to represents this signal
+    pub op: Signal<In, Operation>,
+
     pub o: Signal<Out, Bits<SIZE>>,
 }
 
 impl Logic for ALU {
     #[hdl_gen]
     fn update(&mut self) {
-        self.o.next = self.a.val() + self.b.val();
+        match self.op.val() {
+            Operation::Addition => self.o.next = self.a.val() + self.b.val(),
+            Operation::Substraction => self.o.next = self.a.val() - self.b.val(),
+            Operation::And => self.o.next = self.a.val() & self.b.val(),
+            Operation::Or => self.o.next = self.a.val() | self.b.val(),
+        } 
     } 
 }
 
